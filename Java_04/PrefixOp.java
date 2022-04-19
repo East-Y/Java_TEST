c38ef66c431c18cce14dd7766d2e8683c9c91e4e
East-Y <pomonas@nate.com>
2022-04-19 16:57:08 +0900

    Java_04

---
+++ Java_04/PrefixOp.java
@@ -0,0 +1,10 @@
+package Java_04;
+
+public class PrefixOp {
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println(++num);
+        System.out.println(++num);
+        System.out.println(num);
+    }
+}
