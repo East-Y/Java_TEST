d18a3c2d39cb0cd2c6f8ab1432ab27854019c21c
East-Y <pomonas@nate.com>
2022-04-20 14:30:26 +0900

    Java_05

---
+++ Java_05/WhileBasic.java
@@ -0,0 +1,12 @@
+package Java_05;
+
+public class WhileBasic {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while(num < 5) {
+            System.out.println("I like Java " + num);
+            num++;
+        }
+    }
+}
