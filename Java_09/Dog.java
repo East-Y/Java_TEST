e95e696f44c3e54d686596863a871328fd5f9116
East-Y <pomonas@nate.com>
2022-04-22 17:31:04 +0900

    Java_09

---
+++ Java_09/Dog.java
@@ -0,0 +1,13 @@
+package Java_09;
+
+public class Dog {
+    public void makeCat() {
+        // Cat은 public으로 선언되었으므로 어디서든 인스턴스 생성 가능
+        Cat yaong = new Cat();
+    }
+
+    public void makeDuck() {
+        // Duck은 default로 선언되었으므로 이 위치에서 인스턴스 생성 불가
+        Duck quack = new Duck();
+    }
+}
