bb4e0f8613a7c0427651b48e0ad54393e18e00be
East-Y <pomonas@nate.com>
2022-04-26 11:19:30 +0900

    Java_10

---
+++ Java_10/CarNBoat.java
@@ -0,0 +1,17 @@
+package Java_10;
+
+class Car {
+    void maCar() {
+        System.out.println("This is my car");
+    }
+}
+
+class Boat {
+    void myBoat() {
+        System.out.println("This is my boat");
+    }
+}
+
+public class CarNBoat {
+    
+}
---
+++ Java_10/DateOfExecution.java
@@ -0,0 +1,16 @@
+package Java_10;
+
+import java.time.LocalDate;
+
+class DateOfExecution {
+    static String date;
+
+    static {
+        LocalDate nDate = LocalDate.now();
+        date = nDate.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(date);
+    }
+}
