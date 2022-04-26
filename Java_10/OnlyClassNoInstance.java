0bd270479325f96d2f27509ba638f01941aa07b1
East-Y <pomonas@nate.com>
2022-04-26 09:36:38 +0900

    Java_10

---
+++ Java_10/OnlyClassNoInstance.java
@@ -0,0 +1,17 @@
+package Java_10;
+
+class InstCnt {
+    static int instNum = 100;
+
+    InstCnt() {
+        instNum++;
+        System.out.println("인스턴스 생성 : " + instNum);
+    }
+}
+
+class OnlyClassNoInstance {
+    public static void main(String[] args) {
+        InstCnt.instNum -= 15;
+        System.out.println(InstCnt.instNum);
+    }
+}
